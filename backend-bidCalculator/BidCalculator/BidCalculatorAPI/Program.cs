b846b6604e39e6f64d5d0939ec5ccbd49fdd650f
WhiteRabbitCoder <angelogaviriam@gmail.com>
2025-10-31 05:01:33 -0500

    add routing middleware to Program.cs for improved request handling

--- backend-bidCalculator/BidCalculator/BidCalculatorAPI/Program.cs
+++ backend-bidCalculator/BidCalculator/BidCalculatorAPI/Program.cs
@@ -54,6 +54,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
+app.UseRouting();
+
 app.UseCors(policy => policy
     .AllowAnyOrigin()
     .AllowAnyMethod()
